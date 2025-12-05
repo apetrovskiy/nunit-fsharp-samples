782c2f1d2b64565f8190e7cb948b04db849ec889
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2025-12-05 22:32:36 +0300

    the run everything script

---
+++ run_everything.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+projects=("money" "syntax")
+
+for project in "${projects[@]}"
+do
+    cd "${project}" || exit
+    pwd
+    rm -rf bin/ obj/
+    dotnet clean
+    dotnet restore
+    dotnet build
+    dotnet test
+    cd ..
+done
+
+allure serve report/allure-results/
